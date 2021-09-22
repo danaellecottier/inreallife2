f524987b3694a9c343372b8a1ef03ffd0d471e00
Danaelle Cottier <you@example.com>
2021-09-22 15:34:02 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,210 @@
+const button = document.getElementById('Go');
+const box1 = document.querySelector('.box1');
+const box2 = document.querySelector('.box2');
+const box3 = document.querySelector('.box3');
+const box4 = document.querySelector('.box4');
+const box5 = document.querySelector('.box5');
+const box6 = document.querySelector('.box6');
+const box7 = document.querySelector('.box7');
+const box8 = document.querySelector('.box8');
+const box9 = document.querySelector('.box9');
+const box10 = document.querySelector('.box10');
+const box11 = document.querySelector('.box11');
+const box12 = document.querySelector('.box12');
+const box13 = document.querySelector('.box13');
+const box14 = document.querySelector('.box14');
+const box15 = document.querySelector('.box15');
+const box16 = document.querySelector('.box16');
+const box17 = document.querySelector('.box17');
+const box18 = document.querySelector('.box18');
+const box19 = document.querySelector('.box19');
+const box20 = document.querySelector('.box20');
+const box21 = document.querySelector('.box21');
+const box22 = document.querySelector('.box22');
+
+
+
+function search() {
+    
+    if(box1.value == 'b') {
+        box1.style.backgroundColor = "green";
+    } else if (box1.value == ''){
+        box1.style.backgroundColor = "white";
+    }else {
+        box1.style.backgroundColor = "red";
+    };
+
+    if(box2.value == 'k') {
+        box2.style.backgroundColor = "green";
+    } else if (box2.value == ''){
+        box2.style.backgroundColor = "white";
+    }else {
+        box2.style.backgroundColor = "red";
+    };
+
+    if(box3.value == 'u') {
+        box3.style.backgroundColor = "green";
+    } else if (box3.value == ''){
+        box3.style.backgroundColor = "white";
+    }else {
+        box3.style.backgroundColor = "red";
+    };
+
+    if(box4.value == 'q') {
+        box4.style.backgroundColor = "green";
+    } else if (box4.value == ''){
+        box4.style.backgroundColor = "white";
+    }else {
+        box4.style.backgroundColor = "red";
+    };
+
+    if(box5.value == 'j') {
+        box5.style.backgroundColor = "green";
+    } else if (box5.value == ''){
+        box5.style.backgroundColor = "white";
+    }else {
+        box5.style.backgroundColor = "red";
+    };
+
+    if(box6.value == 'm') {
+        box6.style.backgroundColor = "green";
+    } else if (box6.value == ''){
+        box6.style.backgroundColor = "white";
+    }else {
+        box6.style.backgroundColor = "red";
+    };
+
+    if(box7.value == 't') {
+        box7.style.backgroundColor = "green";
+    } else if (box7.value == ''){
+        box7.style.backgroundColor = "white";
+    }else {
+        box7.style.backgroundColor = "red";
+    };
+
+    if(box8.value == 'h') {
+        box8.style.backgroundColor = "green";
+    } else if (box8.value == ''){
+        box8.style.backgroundColor = "white";
+    }else {
+        box8.style.backgroundColor = "red";
+    };
+
+    if(box9.value == 'i') {
+        box9.style.backgroundColor = "green";
+    } else if (box9.value == ''){
+        box9.style.backgroundColor = "white";
+    }else {
+        box9.style.backgroundColor = "red";
+    };
+
+    if(box10.value == 'd') {
+        box10.style.backgroundColor = "green";
+    } else if (box10.value == ''){
+        box10.style.backgroundColor = "white";
+    }else {
+        box10.style.backgroundColor = "red";
+    };
+
+    if(box11.value == 'n') {
+        box11.style.backgroundColor = "green";
+    } else if (box11.value == ''){
+        box11.style.backgroundColor = "white";
+    }else {
+        box11.style.backgroundColor = "red";
+    };
+
+    if(box12.value == 'w') {
+        box12.style.backgroundColor = "green";
+    } else if (box12.value == ''){
+        box12.style.backgroundColor = "white";
+    }else {
+        box12.style.backgroundColor = "red";
+    };
+
+    if(box13.value == 's') {
+        box13.style.backgroundColor = "green";
+    } else if (box13.value == ''){
+        box13.style.backgroundColor = "white";
+    }else {
+        box13.style.backgroundColor = "red";
+    };
+
+    if(box14.value == 'r') {
+        box14.style.backgroundColor = "green";
+    } else if (box14.value == ''){
+        box14.style.backgroundColor = "white";
+    }else {
+        box14.style.backgroundColor = "red";
+    };
+
+    if(box15.value == 'e') {
+        box15.style.backgroundColor = "green";
+    } else if (box15.value == ''){
+        box15.style.backgroundColor = "white";
+    }else {
+        box15.style.backgroundColor = "red";
+    };
+
+    if(box16.value == 'v') {
+        box16.style.backgroundColor = "green";
+    } else if (box16.value == ''){
+        box16.style.backgroundColor = "white";
+    }else {
+        box16.style.backgroundColor = "red";
+    };
+
+    if(box17.value == 'o') {
+        box17.style.backgroundColor = "green";
+    } else if (box17.value == ''){
+        box17.style.backgroundColor = "white";
+    }else {
+        box17.style.backgroundColor = "red";
+    };
+
+    if(box18.value == 'f') {
+        box18.style.backgroundColor = "green";
+    } else if (box18.value == ''){
+        box18.style.backgroundColor = "white";
+    }else {
+        box18.style.backgroundColor = "red";
+    };
+
+    if(box19.value == 'g') {
+        box19.style.backgroundColor = "green";
+    } else if (box19.value == ''){
+        box19.style.backgroundColor = "white";
+    }else {
+        box19.style.backgroundColor = "red";
+    };
+
+    if(box20.value == 'l') {
+        box20.style.backgroundColor = "green";
+    } else if (box20.value == ''){
+        box20.style.backgroundColor = "white";
+    }else {
+        box20.style.backgroundColor = "red";
+    };
+
+    if(box21.value == 'a') {
+        box21.style.backgroundColor = "green";
+    } else if (box21.value == ''){
+        box21.style.backgroundColor = "white";
+    }else {
+        box21.style.backgroundColor = "red";
+    };
+
+    if(box22.value == 'p') {
+        box22.style.backgroundColor = "green";
+    } else if (box22.value == ''){
+        box22.style.backgroundColor = "white";
+    }else {
+        box22.style.backgroundColor = "red";
+    };
+
+
+
+
+}
+
+button.addEventListener('click', search)
